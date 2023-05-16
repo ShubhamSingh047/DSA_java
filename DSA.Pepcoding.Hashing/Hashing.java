3c263dfa060b122947080a2bc660950fd69e1564
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-17 02:54:35 +0530

    hashing

---
+++ DSA.Pepcoding.Hashing/Hashing.java
@@ -0,0 +1,11 @@
+package DSA.Pepcoding.Hashing;
+import java.util.*;
+public class Hashing {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India",100);
+        hm.put("China",150);
+        hm.put("Us",50);
+        System.out.println(hm);
+    }
+}
