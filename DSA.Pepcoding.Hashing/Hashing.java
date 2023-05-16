3ac4ef4d52c1593792c34fd1e88946deed411680
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-17 03:10:21 +0530

    hashmap functions

--- DSA.Pepcoding.Hashing/Hashing.java
+++ DSA.Pepcoding.Hashing/Hashing.java
@@ -3,9 +3,30 @@ import java.util.*;
 public class Hashing {
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
+
+        //Insert Operations
         hm.put("India",100);
         hm.put("China",150);
         hm.put("Us",50);
         System.out.println(hm);
+
+        //Get Operations (Find)
+        int res=hm.get("India");
+        System.out.println(res);
+
+        //Contains Key return boolean
+        System.out.println(hm.containsKey("India"));
+
+        //remove Operation
+        System.out.println(hm.remove("China"));
+
+        //Size of Hashmap
+        System.out.println(hm.size());
+
+        //check if HashMap is empty
+        System.out.println(hm.isEmpty());
+
+        //Clear our hashmap / delete
+        hm.clear();
     }
 }
