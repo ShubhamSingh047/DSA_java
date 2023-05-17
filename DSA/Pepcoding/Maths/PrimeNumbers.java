47ae1996cfb9f5a0f496996aa37c0ae94141bc7a
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-17 17:29:23 +0530

    prime number

---
+++ DSA/Pepcoding/Maths/PrimeNumbers.java
@@ -0,0 +1,17 @@
+package DSA.Pepcoding.Maths;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=2; i*i<n; i++){
+            if(n%i==0){
+                System.out.println(false);
+                return;
+            }
+        }
+        System.out.println(true);
+    }
+}
