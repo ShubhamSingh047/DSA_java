834841a7bb1b1bab895b51b51d5bb296742679f7
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-19 04:58:19 +0530

    distinct elment

---
+++ DSA/Pepcoding/Stack/DublicateBracket.java
@@ -0,0 +1,14 @@
+package DSA.Pepcoding.Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DublicateBracket {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        Stack<Character>st = new Stack<>();
+
+    }
+}
---
+++ DSA/Pepcoding/Stack/Testing.java
@@ -0,0 +1,13 @@
+package DSA.Pepcoding.Stack;
+
+import java.util.Stack;
+
+public class Testing {
+    public static void main(String[] args) {
+        Stack<Integer> i = new Stack<>();
+        i.push(10);
+        i.push(20);
+        i.push(30);
+        System.out.println(i);
+    }
+}
