62ac1f5de1878ac30cb1d583b1ade9363a1276b2
Shubham <shubham.shubham.singh13@gmail.com>
2023-05-21 01:04:35 +0530

    UnNessary Brackets

--- DSA/Pepcoding/Stack/DublicateBracket.java
+++ DSA/Pepcoding/Stack/DublicateBracket.java
@@ -4,11 +4,27 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DublicateBracket {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
 
         Stack<Character>st = new Stack<>();
-
+        
+        for(int i=0; i<str.length(); i++){
+            char ch=str.charAt(i);
+            if(ch==')'){
+                if(st.peek()=='('){
+                    System.out.println(true);
+                    return;
+                }else{
+                    while(st.peek()!='('){
+                        st.pop();
+                    }
+                }
+            }else{
+                st.push(ch);
+            }
+        }
+        System.out.println(false);
     }
 }
